ef115a20fde5479b08c22ad97125604fd4ee9397
Nathan Jensen <nath.h.jensen@gmail.com>
2025-05-11 04:48:24 -0600

    updates or something

--- realsense_ros2/launch/realsense_2d_slam_launch.py
+++ realsense_ros2/launch/realsense_2d_slam_launch.py
@@ -10,14 +10,14 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='realsense_ros2',
-            node_executable='rs_t265_node',
-            node_name='rs_t265',
+            executable='rs_t265_node',
+            name='rs_t265',
             output='screen'
         ),
         Node(
             package='realsense_ros2',
-            node_executable='rs_d435_node',
-            node_name='rs_d435',
+            executable='rs_d435_node',
+            name='rs_d435',
             output='screen',
             parameters=[
                 {"publish_depth": True},
@@ -31,20 +31,20 @@ def generate_launch_description():
         Node(
             ## Configure the TF of the robot 
             package='tf2_ros',
-            node_executable='static_transform_publisher',
+            executable='static_transform_publisher',
             output='screen',
             arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 't265_frame', 'base_link']
         ),
         Node(
             package='tf2_ros',
-            node_executable='static_transform_publisher',
+            executable='static_transform_publisher',
             output='screen',
             arguments=['0.0', '0.025', '0.03', '0.0', '0.0', '0.0', 'base_link', 'camera_link_d435']
             ),
 
         Node(
             package='tf2_ros',
-            node_executable='static_transform_publisher',
+            executable='static_transform_publisher',
             output='screen',
             arguments=['0.0', '0.025', '0.03', '-1.5708', '0.0', '-1.5708', 'base_link', 'camera_link_d435_pcl']
             # arguments=['0.0', '0.025', '0.03', '0.0', '0.0', '0.0', 'base_link', 'camera_link_d435_pcl']
@@ -53,8 +53,8 @@ def generate_launch_description():
             
         Node(
             package='depthimage_to_laserscan',
-            node_executable='depthimage_to_laserscan_node',
-            node_name='scan',
+            executable='depthimage_to_laserscan_node',
+            name='scan',
             output='screen',
             parameters=[{'output_frame':'camera_link_d435'}],
             remappings=[('depth','rs_d435/aligned_depth/image_raw'),
